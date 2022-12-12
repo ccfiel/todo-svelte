7155963b7bd0248192f8d57194b7db0350810b49
Chris Ian Fiel <ccfiel@gmail.com>
2022-12-12 17:42:51 +0800

    1st

---
+++ src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    let counter = 0;
+    let new_todo = "";
+    function addToDo() {
+        counter += 1;
+        $todos = [...$todos, { id: counter, description: new_todo, check: false}];
+        new_todo = "";
+    }
+
+
+    import Todo from "./Todo.svelte";
+    import { todos } from "./store";
+</script>
+
+<input bind:value={new_todo}>
+<button on:click={addToDo}> Add To Do</button>
+
+<br/>
+{#each $todos as dos}
+    <Todo  {...dos} ></Todo>
+
+{/each}
---
+++ src/routes/Todo.svelte
@@ -0,0 +1,34 @@
+<script>
+    export let id = 0;
+    export let check = false;
+    export let description = "";
+
+   let value = check;
+
+    import { todos } from "./store";
+    function deleteToDo() {
+        $todos = $todos.filter(obj => obj.id !== id);
+        console.log("11111111")
+        console.log($todos);
+    }
+
+    function completeToDo() {
+        console.log("22222222");
+        for (const i of $todos) {
+            if (i.id == id) {
+                i.check = value;
+            }
+        }
+    }
+</script>
+
+<label>
+	<input type=checkbox bind:checked={value} on:change={completeToDo}>
+    {#if value}
+	    <s>{description}</s>
+    {:else}
+	    {description}
+    {/if}
+    <button on:click={deleteToDo}>Delete</button>
+    <br/>
+</label>
